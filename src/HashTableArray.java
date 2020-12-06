6c4b2746ddd7281ba0ebfb41cbf1d8fe9d56cb42
alcns <alican.sariboga.acs@gmail.com>
2020-12-06 15:23:38 +0000

    Some Fixed

--- src/HashTableArray.java
+++ src/HashTableArray.java
@@ -1,4 +1,4 @@
-public class HashTableArray {
+public class HashTableArray<T> {
 
     Entry[] arrayHash;
     int size;
@@ -10,10 +10,29 @@ public class HashTableArray {
             arrayHash[i] = new Entry();
         }
     }
-    int getHash(int key){
-        return key%size;
-    }
-    public void put(){
 
+    int getHash(int key) {
+        return key % size;
+    }
+
+    public void put(int key, Object value) {
+        int hashIndex = getHash(key);
+        Entry arrayValue = arrayHash[hashIndex];
+        Entry newItem = new Entry(key, value);
+        newItem.next = arrayValue.next;
+        arrayValue.next = newItem;
+
+    }
+
+    public T get(int key) {
+        T value = null;
+        int hashIndex = getHash(key);
+        Entry arrayValue = arrayHash[hashIndex];
+        while(arrayValue != null ){
+            if (arrayValue.getKey() == key){
+                value = (T) arrayValue.getValue();
+            }
+        }
+        return value;
     }
 }
